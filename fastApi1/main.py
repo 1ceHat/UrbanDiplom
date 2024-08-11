16c0a5a432917a52979d02efc80ae0dbae601332
1ceHat <artemdudorov2015@gmail.com>
2024-08-11 12:51:18 +0500

    update template FastAPI
    update template plus fix bugs

--- fastApi1/main.py
+++ fastApi1/main.py
@@ -1,4 +1,6 @@
 import uvicorn
+from pprint import pprint
+
 from fastapi import FastAPI, HTTPException, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi_pagination import add_pagination, paginate, Params
@@ -47,14 +49,12 @@ async def post_signup_page(request: Request,
                            db: Annotated[Session, Depends(get_db)]):
     global curr_user
     check_user = db.scalars(select(Buyer).where(Buyer.name == username)).first()
-    info = {}
+    error = {}
     if curr_user is None:
         if check_user is not None:
-            info.update({'error': 'Пользователь с таким именем существует',
-                         'message': 'У вас уже есть аккаунт?',
-                         'login': 'Авторизуйтесь!'})
+            error.update({'error': 'Пользователь с таким именем существует'})
         elif password != repeat_password:
-            info.update({'error': 'Пароли не совпадают'})
+            error.update({'error': 'Пароли не совпадают'})
         else:
             db.execute(insert(Buyer).values(name=username,
                                             password=password,
@@ -65,7 +65,7 @@ async def post_signup_page(request: Request,
 
     context = {
         'request': request,
-        'info': info,
+        'error': error,
         'user': curr_user,
     }
     return templates.TemplateResponse('registration_page.html', context=context)
@@ -87,81 +87,118 @@ async def post_login_page(request: Request,
                           password: Annotated[str, Form()],
                           db: Annotated[Session, Depends(get_db)]):
     global curr_user
-    info = {}
+    error = {}
     if curr_user is None:
         check_user = db.scalars(select(Buyer).where(Buyer.name == username)).first()
         if check_user is None:
-            info.update({'error': 'Такого пользователя не существует',
-                         'message': 'У вас ещё нет аккаунта?',
-                         'signup': 'Зарегистрируйтесь!'})
+            error.update({'error': 'Такого пользователя не существует'})
         elif check_user.password != password:
-            info.update({'error': 'Неверный пароль'})
+            error.update({'error': 'Неверный пароль'})
         else:
             curr_user = check_user
             return RedirectResponse('/', status_code=302)
 
     context = {
         'request': request,
-        'info': info,
+        'error': error,
         'user': curr_user,
     }
     return templates.TemplateResponse('login_page.html', context=context)
 
 
 @app.get('/purchased_applications')
-async def get_purchased_applications(request: Request, db: Annotated[Session, Depends(get_db)]) -> HTMLResponse:
+async def get_purchased_applications(request: Request,
+                                     db: Annotated[Session, Depends(get_db)],
+                                     page: int = 1,) -> HTMLResponse:
     global curr_user
-    applications = []
+    games = []
+    error = {}
     if curr_user:
         curr_user = db.get(Buyer, curr_user.id)
-        applications = curr_user.buyers_game
+        games = curr_user.buyers_game
+    else:
+        error.update({'error': 'Вы не авторизованы. Пожалуйста, войдите в аккаунт или зарегистрируйтесь.'})
+
+    params = Params(size=5, page=page)
+    paginated_games = paginate(games, params)
     context = {
         'request': request,
         'user': curr_user,
-        'applications': applications,
+        'paginator': paginated_games,
+        'page_obj': {
+            'items': paginated_games.items,
+            'number': paginated_games.page,
+            'has_other_pages': True if paginated_games.pages > 1 else False,
+            'has_previous': True if paginated_games.page - 1 > 0 else False,
+            'has_next': True if paginated_games.page + 1 <= paginated_games.pages else False,
+            'paginator': {'page_range': range(1, paginated_games.pages + 1)},
+            'previous_page_number': paginated_games.page - 1,
+            'next_page_number': paginated_games.page + 1,
+        },
+        'size': paginated_games.size,
+        'error': error,
     }
     return templates.TemplateResponse('users_game_page.html', context=context)
 
 
-@app.get('/shop')
+@app.get('/shop/')
 async def get_shop_page(request: Request,
                         db: Annotated[Session, Depends(get_db)],
                         page: int = 1, size: int = 3) -> HTMLResponse:
     global curr_user
     games = db.scalars(select(Game)).all()
-    params = Params(size=int(size), page=page)
+    params = Params(size=size, page=page)
     paginated_games = paginate(games, params)
+
+    if page > paginated_games.pages:
+        paginated_games = paginate(games, Params(size=size, page=paginated_games.pages))
+
     context = {
         'request': request,
         'user': curr_user,
-        'games': paginated_games,
-        'size': size,
+        'paginator': paginated_games,
+        'page_obj': {
+            'items': paginated_games.items,
+            'number': paginated_games.page,
+            'has_other_pages': True if paginated_games.pages > 1 else False,
+            'has_previous': True if paginated_games.page-1 > 0 else False,
+            'has_next': True if paginated_games.page+1 <= paginated_games.pages else False,
+            'paginator': {'page_range': range(1, paginated_games.pages+1)},
+            'previous_page_number': paginated_games.page-1,
+            'next_page_number': paginated_games.page+1,
+        },
+        'size': paginated_games.size,
         'info': {},
     }
     return templates.TemplateResponse('shop_page.html', context=context)
 
 
-@app.post('/shop')
+@app.post('/shop/')
 async def post_shop_page(request: Request,
                          db: Annotated[Session, Depends(get_db)],
-                         game_title: Annotated[str, Form()],
-                         size: Annotated[int, Form()], page: Annotated[int, Form()]) -> HTMLResponse:
+                         game_id: Annotated[int, Form()],
+                         size: int = 3, page: int = 1) -> HTMLResponse:
     global curr_user
-    game = db.scalars(select(Game).where(Game.title == game_title)).first()
+    game = db.scalars(select(Game).where(Game.id == game_id)).first()
     games = db.scalars(select(Game)).all()
     info = {}
+    error = {}
     params = Params(size=int(size), page=page)
     paginated_games = paginate(games, params)
+
+    if page > paginated_games.pages:
+        paginated_games = paginate(games, Params(size=size, page=paginated_games.pages))
+
     if not curr_user:
-        info.update({'error': 'Войдите в аккаунт или зарегистрируйтесь!'})
+        error.update({'error': 'Войдите в аккаунт или зарегистрируйтесь!'})
     else:
         curr_user = db.get(Buyer, curr_user.id)
         if curr_user.balance < game.cost:
-            info.update({'error': 'Недостаточно средств'})
+            error.update({'error': 'Недостаточно средств'})
         elif game.age_limited and curr_user.age < 18:
-            info.update({'error': 'Вы не достигли возраста покупки'})
+            error.update({'error': 'Вы не достигли возраста покупки'})
         elif game in curr_user.buyers_game:
-            info.update({'error': 'У Вас уже куплена эта игра'})
+            error.update({'error': 'У Вас уже куплена эта игра'})
         else:
             curr_user.balance -= game.cost
             curr_user.buyers_game.append(game)
@@ -170,9 +207,20 @@ async def post_shop_page(request: Request,
     context = {
         'request': request,
         'user': curr_user,
-        'games': paginated_games,
-        'size': size,
-        'info': info,
+        'paginator': paginated_games,
+        'page_obj': {
+            'items': paginated_games.items,
+            'number': paginated_games.page,
+            'has_other_pages': True if paginated_games.pages > 1 else False,
+            'has_previous': True if paginated_games.page-1 > 0 else False,
+            'has_next': True if paginated_games.page+1 <= paginated_games.pages else False,
+            'paginator': {'page_range': range(1, paginated_games.pages+1)},
+            'previous_page_number': paginated_games.page-1,
+            'next_page_number': paginated_games.page+1,
+        },
+        'size': paginated_games.size,
+        'info': {},
+        'error': error,
     }
     return templates.TemplateResponse('shop_page.html', context=context)
 
